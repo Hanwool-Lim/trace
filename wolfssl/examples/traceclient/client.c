6441260c4d8f16f04b79c3d9d3969e35ccd50945
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-06-26 20:17:47 +0900

    Update client.c

--- wolfssl/examples/traceclient/client.c
+++ wolfssl/examples/traceclient/client.c
@@ -32,7 +32,7 @@
 #include <wolfssl/openssl/ssl.h> //추가
 //wolfssl/openssl/ssl.h은 cyassl/openssl/ssl.h과 비교하여 많은 코드가 존재
 
-//추가된 항목
+//추가된 항목 //cyassl/error-ssl.h 역시 wolfssl을 include하고있음
 #ifdef WOLFSSL_DTLS
     #include <wolfssl/error-ssl.h>
 #endif
