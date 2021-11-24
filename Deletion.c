84a15a18945425210c39f634035f5449f51fdcdd
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-11-24 17:20:41 +0900

    Add files via upload

--- Deletion.c
+++ Deletion.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char* argv[]){
 
-	char server_link[100] = "192.168.0.64:/home/test/test";
-	char nfs_link[100] = "/home/tracking/test2";
+//	char server_link[100] = "192.168.219.102:/home/jun/Desktop/IOagent";
+	char nfs_link[100] = "/home/tracking/Desktop/IOagent";
 	char command[100];
 	int count;
 
-	sprintf(command, "sudo mount -t nfs4 -o minorversion=1 %s %s", server_link, nfs_link);
+//	sprintf(command, "sudo mount -t nfs4 -o minorversion=1 %s %s", server_link, nfs_link);
+//	system(command);
+
+	sprintf(command, "rm -rf %s/%s", nfs_link, argv[1]);
 	system(command);
 
-	sprintf(command, "sudo srm -z %s/%s", nfs_link, argv[1]);
-	system(command);
-
-	sprintf(command, "sudo umount -l %s", nfs_link);
-	system(command);
+//	sprintf(command, "sudo umount -l %s", nfs_link);
+//	system(command);
 
 	return 0;
 }
