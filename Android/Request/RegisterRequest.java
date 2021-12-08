66b0dd325591f0b2104a217366456921f1b2bd0c
ByounWonJun <won_jan@hanmail.net>
2021-12-08 16:42:46 +0900

    Add files via upload

---
+++ Android/Request/RegisterRequest.java
@@ -0,0 +1,32 @@
+package com.example.tracking.Request;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+
+    final static private String URL = "http://122.46.129.53:727/Register.php";
+    private Map<String, String> map;
+
+    public RegisterRequest(String UserID, String UserPassword, String UserName, int UserPhone, int UserNumber, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("UserID",UserID);
+        map.put("UserPassword",UserPassword);
+        map.put("UserName",UserName);
+        map.put("UserPhone",UserPhone + "");
+        map.put("UserNumber",UserNumber + "");
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
