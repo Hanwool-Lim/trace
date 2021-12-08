a18a17715b474c04972c846577b0c001945afa01
ByounWonJun <won_jan@hanmail.net>
2021-12-08 16:41:44 +0900

    Add files via upload

---
+++ Android/Request/DeleteDeviceRequest.java
@@ -0,0 +1,29 @@
+package com.example.tracking.Request;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeleteDeviceRequest extends StringRequest {
+
+    final static private String URL = "http://122.46.129.53:727/DeviceDelete.php";
+    private Map<String, String> map;
+
+    public DeleteDeviceRequest(String DeviceID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("DeviceID",DeviceID);
+
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
