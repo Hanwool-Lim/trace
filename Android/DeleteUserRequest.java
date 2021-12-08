6d12499d7315e3ac8c3d0a1410bb4604e9b0861b
ByounWonJun <won_jan@hanmail.net>
2021-12-08 16:33:17 +0900

    Add files via upload

---
+++ Android/DeleteUserRequest.java
@@ -0,0 +1,29 @@
+package com.example.tracking;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeleteUserRequest extends StringRequest {
+
+    final static private String URL = "http://192.168.0.26/UserDelete.php";
+    private Map<String, String> map;
+
+    public DeleteUserRequest(String UserID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("UserID",UserID);
+
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
