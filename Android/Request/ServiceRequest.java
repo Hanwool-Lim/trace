e7e43c576c8a62c4efb8d2223ff03c2884596ab9
ByounWonJun <won_jan@hanmail.net>
2021-12-08 16:43:00 +0900

    Add files via upload

---
+++ Android/Request/ServiceRequest.java
@@ -0,0 +1,28 @@
+package com.example.tracking.Request;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceRequest extends StringRequest {
+
+    final static private String URL = "http://122.46.129.53:727/Service.php";
+    private Map<String, String> map;
+
+    public ServiceRequest(String DeviceID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("DeviceID",DeviceID);
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
