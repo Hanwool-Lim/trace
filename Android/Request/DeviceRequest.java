ab08c43a65f08316ce58fb31d75273c3b640d145
ByounWonJun <won_jan@hanmail.net>
2021-12-08 16:42:04 +0900

    Add files via upload

---
+++ Android/Request/DeviceRequest.java
@@ -0,0 +1,28 @@
+package com.example.tracking.Request;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeviceRequest extends StringRequest {
+
+    final static private String URL = "http://122.46.129.53:727/Device.php";
+    private Map<String, String> map;
+
+    public DeviceRequest(String UserID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("UserID",UserID);
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
