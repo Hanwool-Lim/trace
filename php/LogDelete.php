1d40a3ffb6926ac0015ee07849d4b9f7dca92db1
ByounWonJun <won_jan@hanmail.net>
2021-12-17 15:21:42 +0900

    Update LogDelete.php

--- php/LogDelete.php
+++ php/LogDelete.php
@@ -17,6 +17,8 @@
 		$command = $row["FileID"];
 		system("python Socket.py /home/tracking/trace/Deletion '$command'");
 		system("/home/tracking/trace/Deletion '$command'");
+		
+		$statement = mysqli_prepare($con, "INSERT INTO removed values ('$DeviceID','$ServiceID','$command')");
    	}
 
 	    #로그 삭제
