2e6ac5369983c8107e7006446e29004aa519ee76
ByounWonJun <won_jan@hanmail.net>
2021-12-17 15:24:24 +0900

    Update AppDelete.php

--- php/AppDelete.php
+++ php/AppDelete.php
@@ -19,6 +19,7 @@
 			
 			$statement = mysqli_prepare($con, "INSERT INTO removed values ('$DeviceID','$ServiceID','$command')");
 			mysqli_stmt_bind_param($statement, "sss", $Date, $DeviceID, $ServiceID);
+			mysqli_stmt_execute($statement);
 		}
 
 		#맵핑만 
